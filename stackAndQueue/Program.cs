05e3f33fca018953aca48d72ddda25f0f6097fa4
000VIJAY <vijay893689@gmail.com>
2022-06-30 15:32:22 +0530

    Added Program for Create using Stack

--- stackAndQueue/Program.cs
+++ stackAndQueue/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Welcome to Program for stack and queue");
+﻿using stackAndQueue;
+Stack GetStack = new Stack();
+GetStack.Push(70);
+GetStack.Push(30);
+GetStack.Push(56);
+GetStack.Display();
---
+++ stackAndQueue/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackAndQueue
+{
+    public class Stack
+    {
+        Node data;
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (data == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = data;
+            }
+            data = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = data;
+            while (temp != null)
+            {
+                Console.WriteLine("data: " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
